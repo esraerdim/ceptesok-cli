ac1b5530c40061e49eaadfa3789c35e24366e1a7
brkyselim <berkayselim97@gmail.com>
2019-12-10 17:26:19 +0300

    pagination buttons bg active

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -2,11 +2,11 @@
 <div>
     <nav class="section listing-pagination">
         <ul class="pagination-elements">
-            <li class="pagination-element active"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:1 }}" class="pagination-link">1</router-link></li>
-            <li class="pagination-element"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:2 }}" class="pagination-link">2</router-link></li>
-                <li class="pagination-element"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:3 }}" class="pagination-link">3</router-link></li>
-                <li class="pagination-element"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:4 }}" class="pagination-link">4</router-link></li>
-                <li class="pagination-element"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:5 }}" class="pagination-link">5</router-link></li>
+            <li class="pagination-element active" v-on:click="changeActiveP($event)"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:1 }}" class="pagination-link">1</router-link></li>
+            <li class="pagination-element" v-on:click="changeActiveP($event)"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:2 }}" class="pagination-link">2</router-link></li>
+                <li class="pagination-element" v-on:click="changeActiveP($event)"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:3 }}" class="pagination-link">3</router-link></li>
+                <li class="pagination-element" v-on:click="changeActiveP($event)"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:4 }}" class="pagination-link">4</router-link></li>
+                <li class="pagination-element" v-on:click="changeActiveP($event)"><router-link :to="{ path: `${this.$route.fullPath}`, query: { page:5 }}" class="pagination-link">5</router-link></li>
         </ul>
     </nav>
 </div>
@@ -16,9 +16,18 @@
 export default {
     name:'Sayfalar',
     data (){
-        return{}
+        return{
+            isActive:false,
+        }
     },
     methods: {
+        changeActiveP(e){
+            var list = document.querySelectorAll("#app > div > div > main > div:nth-child(2) > nav > ul")[0].children;
+            for(var i=0;i<list.length;i++){
+                list[i].className="pagination-element"
+            }
+            e.path[1].className="pagination-element active"
+        },
         getanyw:function(){
             console.log(this.$route.query)
         }
